a6cbeb561be347989092d2b2d60dee8c67e4be02
AmyTroutman <amyrtroutman@gmail.com>
2020-06-16 19:14:57 -0500

    added data folder and data

---
+++ src/app/data/book-data.ts
@@ -0,0 +1,19 @@
+import {IBook} from '../interface/ibook';
+const Books: IBook[] = [
+{
+title: 'The Hobbit',
+author: 'JRR Tolkien',
+pages: 700
+},
+{
+title: 'The Hobbit',
+author: 'JRR Tolkien',
+pages: 700
+},
+{
+title: 'The Hobbit',
+author: 'JRR Tolkien',
+pages: 700
+}];
+
+export {Books}
